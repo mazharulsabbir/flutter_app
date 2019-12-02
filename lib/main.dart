8471a8d9095d83cfa4c95a923225e9c7f6dee2b1
Rs Robin <srrobin33@gmail.com>
2019-12-02 13:33:20 +0600

    button

--- lib/main.dart
+++ lib/main.dart
@@ -1,94 +1,62 @@
 import 'package:flutter/material.dart';
-import 'package:fluttertoast/fluttertoast.dart';
 
+void main() => runApp(new MyApp());
 
-
-void main(){
-  runApp(
-      MaterialApp(
-        title: 'My App',
-        showSemanticsDebugger: false,
-        home: HomePage(),
-  ));
-
-
-}
-class HomePage extends StatelessWidget{
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'My App',
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text('DashBoard'),
-        ),
-
-        body:Container(
-          color: Colors.tealAccent,
-           width: double.infinity,
-           height: double.infinity,
-           child: Column(
-              children: <Widget>[
-                Center(
-                  child: Text("Calculator",style: TextStyle(fontSize: 24,color: Colors.red),),
-                ),
-                Row(children: <Widget>[
-                   RaisedButton(
-                     textColor: Colors.white,
-                     color: Colors.tealAccent,
-                     child: Text(
-                       "1", style: TextStyle(fontSize: 22,)),
-
-                     shape: RoundedRectangleBorder(borderRadius:new BorderRadius.circular(5)),
-                  ),
-                  RaisedButton(
-                     textColor: Colors.white,
-                     color: Colors.tealAccent,
-                     child: Text(
-                       "2", style: TextStyle(fontSize: 22,)),
-
-                     shape: RoundedRectangleBorder(borderRadius:new BorderRadius.circular(5)),
-                    onPressed: showToast(context,"2"),
-                  ),
-                  FlatButton(
-
-                    textTheme: ButtonTextTheme.primary,
-                     child: Text(
-                       "3", style: TextStyle(fontSize: 22,)),
-
-                     shape: RoundedRectangleBorder(borderRadius:new BorderRadius.circular(5)),
-                    disabledColor: Colors.red,
-                    onPressed: showToast(context,"3"),
-
-                  ),
-
-                ],
-
-                )
-
-
-              ],
-
-
-            )
-        )
-
-             ),
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(),
     );
   }
-
-  showToast(context, msg) {
-    Fluttertoast.showToast(
-        msg: msg,
-        textColor: Colors.white,
-        toastLength: Toast.LENGTH_SHORT,
-        timeInSecForIos: 1,
-        gravity: ToastGravity.BOTTOM,
-        backgroundColor: Colors.indigo,
-    );
-  }
-
 }
 
+class MyHomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _myAppState();
+  }
+}
+
+class _myAppState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text("Home Page"),
+            centerTitle: true,
+          ),
+          body: Container(
+            child: Column(
+              children: <Widget>[
+                Center(
+                  child: Text(
+                    "1",
+                    style: TextStyle(fontSize: 22),
+                  ),
+                ),
+                Center(
+                  child: RaisedButton(
+                    color: Colors.red,
+                    child: Text("Click"),
+                    onPressed: _clickme(),
+                  ),
+                ),
+              ],
+            ),
+          )),
+    );
+  }
+
+  _clickme() {
+    print("Click Button");
+  }
+}
