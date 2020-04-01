03406772d1807ec04b5f13721533173075d5e121
Rs Robin <srrobin33@gmail.com>
2020-04-02 00:24:25 +0600

    comment

--- lib/FoodDetails.dart
+++ lib/FoodDetails.dart
@@ -12,6 +12,7 @@ class FoodDetails extends StatefulWidget {
 
 class _FoodHomeState extends State<FoodDetails> {
   List details;
+  List item_comment=["hello","Hello","Commnet"];
 
   _FoodHomeState(this.details);
 
@@ -97,72 +98,77 @@ class _FoodHomeState extends State<FoodDetails> {
                 ),
               ),
             ),
-            Container(
-              color: Colors.lightBlueAccent.withOpacity(.5),
-              width: double.infinity,
-              child: Row(
-                children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
 
-                  Container(
-                    padding: EdgeInsets.all(4),
-                    width: MediaQuery.of(context).size.width*.15,
-                      color: Colors.red,
-                      child: Icon(
-                        Icons.favorite,
-                        size: 32,
-                        color: Colors.black54,
-                      )),
-                  GestureDetector(
-                    onTap: (){
-                      showBottomsheet();
-                    },
-                    child: Container(
+                Container(
+                  padding: EdgeInsets.all(4),
+                  width: MediaQuery.of(context).size.width*.15,
+                    color: Colors.red,
+                    child: Icon(
+                      Icons.pan_tool,
+                      size: 32,
+                      color: Colors.black54,
+                    )),
+                GestureDetector(
+                  onTap: (){
+                    showBottomsheet();
+                  },
+                  child: Container(
 
-                        padding: EdgeInsets.all(4),
-
-                        width: MediaQuery.of(context).size.width*.7,
-
-                        color: Colors.amber,
-                        child: Row(
-                          children: <Widget>[
-                            Icon(
-                              Icons.comment,
-                              size: 32,
-                              color: Colors.black54,
-                            ),
-                            Text("Comments",style: TextStyle(fontSize: 18),)
-                          ],
-                        )),
-                  ),
-                  Container(
                       padding: EdgeInsets.all(4),
 
-                      width: MediaQuery.of(context).size.width*.15,
+                      width: MediaQuery.of(context).size.width*.7,
 
-                      color: Colors.red,
-                      child: Icon(
-                        Icons.favorite,
-                        size: 32,
-                        color: Colors.black54,
+                      color: Colors.amber,
+                      child: Row(
+                        children: <Widget>[
+                          Icon(
+                            Icons.comment,
+                            size: 32,
+                            color: Colors.black54,
+                          ),
+                          Text("Comments",style: TextStyle(fontSize: 18),)
+                        ],
                       )),
-                ],
-              ),
+                ),
+                Container(
+                    padding: EdgeInsets.all(4),
+
+                    width: MediaQuery.of(context).size.width*.15,
+
+                    color: Colors.red,
+                    child: Icon(
+                      Icons.favorite,
+                      size: 32,
+                      color: Colors.black54,
+                    )),
+              ],
             ),
           ]),
         ),
       ],
     ));
   }
-
   void showBottomsheet() {
     showModalBottomSheet(context: context, builder: (context){
       return Container(
-        color: Colors.amber,
         child: Column(
           children: <Widget>[
-            TextField(
-              decoration: new InputDecoration.collapsed(
-                  hintText: 'Comment'
+            Container(
+              height: MediaQuery.of(context).size.height*.8,
+              child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                itemCount: 50,
+                itemBuilder: (BuildContext context, int index) {
+                  return GestureDetector(
+                      onTap: () {
+                         },
+                  child: listContainer(context, Icons.account_circle,"Name","Comment" ));
+
+                },
               ),
             ),
           ],
@@ -171,3 +177,52 @@ class _FoodHomeState extends State<FoodDetails> {
     });
   }
 }
+
+listContainer(BuildContext context, IconData comment_image, String name, String comment) {
+  return Card(
+    child: Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.all(5.0),
+                width: 35.0,
+                height: 35.0,
+                child: Icon(comment_image),
+              ),
+              Column(
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.70,
+                    child: Text(
+                      name,
+                      overflow: TextOverflow.fade,
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18),
+                    ),
+                  ),
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.70,
+                    child: Text(
+                      comment,
+                      overflow: TextOverflow.fade,
+                      style: TextStyle(
+                          color: Colors.black54,
+                          fontSize: 12),
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+
+        ],
+      ),
+    ),
+  );
+}
+
